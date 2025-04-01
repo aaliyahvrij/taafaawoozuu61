585e85872a9eb8a9ad4c6827351f72129004fb41
Stephanie Appiah Opoku <stephanie.appiah.opoku@hva.nl>
2025-04-01 14:16:26 +0200

    FEAT: change some methods

--- java/src/main/java/com/voteU/election/java/reader/DutchElectionReader.java
+++ java/src/main/java/com/voteU/election/java/reader/DutchElectionReader.java
@@ -15,7 +15,6 @@ import java.util.Map;
  * A very small demo of how the classes {@link DutchElectionProcessor} and {@link Transformer}
  * can be used to process the XML-files.
  * <br>
- * <b>Please do NOT include this code in you project!</b>
  */
 public class DutchElectionReader {
 
@@ -25,14 +24,14 @@ public class DutchElectionReader {
         DutchElectionTransformer transformer = new DutchElectionTransformer();
 
         // And the election processor that traverses the folders and processes the XML-files.
-        DutchElectionProcessor<ArrayList<Election>> electionProcessor = new DutchElectionProcessor<>(transformer);
+        DutchElectionProcessor<Election> electionProcessor = new DutchElectionProcessor<>(transformer);
 
         // Assuming the election data is contained in {@code src/main/resource} it should be found.
         // Please note that you can also specify an absolute path to the folder!
 
-        String[] electionYears = {"TK2021","TK2023"};
+        String[] electionYears = {"TK2021", "TK2023"};
         // Lijst met categorieën (gemeente, kandidatenlijst, kieskring)
-        String[] categories = {"gemeente", "kandidatenlijst"};
+        String[] categories = {"gemeente", "kandidatenlijst", "kieskring"};
 
         // Doorloop elk jaar en elke categorie
         for (String year : electionYears) {
@@ -41,7 +40,7 @@ public class DutchElectionReader {
 
                 try {
                     System.out.println("Processing: " + folderPath);
-                    ArrayList<Election> election = electionProcessor.processResults(year, PathUtils.getResourcePath(folderPath));
+                    Election election = electionProcessor.processResults(year, PathUtils.getResourcePath(folderPath));
                     System.out.println(election);
 
                     System.out.println("Finished processing: " + folderPath);
@@ -61,6 +60,9 @@ public class DutchElectionReader {
         }
 
 
+
     }
+
+
 
 }
