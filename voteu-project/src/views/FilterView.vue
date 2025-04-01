5011f26bced781dc32568fd9fde982ee9b6e6131
Aaliyah <aaliyah.vrij@hva.nl>
2025-04-01 15:05:59 +0200

    fast commit

--- voteu-project/src/views/FilterView.vue
+++ voteu-project/src/views/FilterView.vue
@@ -1,10 +1,8 @@
 <script setup lang="ts">
 
-import NavBarDesktop from "@/components/NavBarDesktop.vue";
 </script>
 
 <template>
-  <NavBarDesktop />
 <h1>Filters</h1>
   <p> filter by what you need to read the election data</p>
 </template>
