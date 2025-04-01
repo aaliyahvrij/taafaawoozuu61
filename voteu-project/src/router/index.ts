d2f057cca69dc75906406af2f7d7412c3753ae5d
Aaliyah <aaliyah.vrij@hva.nl>
2025-04-01 13:31:52 +0200

    Feat: added link to filterview

--- voteu-project/src/router/index.ts
+++ voteu-project/src/router/index.ts
@@ -17,6 +17,11 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    {
+      path: '/filter',
+      name: 'filter',
+      component: () => import('../views/FilterView.vue'),
+    },
   ],
 })
 
