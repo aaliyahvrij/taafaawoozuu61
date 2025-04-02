fcb48d0b96826d0458e0f8f081f17400e9a3ac93
Nigel <agyeman2@hva.nl>
2025-04-03 01:53:05 +0200

    feat: started on yearFilter component frontend

---
+++ voteu-project/src/components/YearFilter.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import {type Ref, ref} from "vue";
+
+const year: Ref<string, string> = ref('');
+</script>
+
+<template>
+  <div class="main-container">
+    <div class="filter-tag">
+      select year
+      <select v-model="year">
+        <option value="2021">2021</option>
+        <option value="2023">2023</option>
+      </select>
+    </div>
+
+    <div class="data-container">
+      Showing parties of : {{year}}
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.main-container{
+  border:1px solid black;
+}
+
+.filter-tag{
+  border:1px solid black;
+  width: 200px;
+  height:50px
+}
+
+select{
+  width: 100%;
+}
+
+.data-container{
+  height: 500px;
+  border:1px solid black;
+}
+
+</style>
---
+++ voteu-project/src/views/FilterView.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+import YearFilter from "@/components/YearFilter.vue";
+</script>
+
+<template>
+  <div class="main-filter-container">
+    <YearFilter />
+  </div>
+
+</template>
+
+<style scoped>
+template{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.main-filter-container{
+  color:black;
+  background-color: #fafafa;
+  width: 800px;
+  height: 600px;
+}
+</style>
