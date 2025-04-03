db8b4cf0d37a74dc911770771d298c1bb5736bb9
Stephanie Appiah Opoku <stephanie.appiah.opoku@hva.nl>
2025-04-03 09:42:50 +0200

    FEAT: added a interface

--- voteu-project/src/services/ConstituencyService.ts
+++ voteu-project/src/services/ConstituencyService.ts
@@ -4,9 +4,12 @@ import type { AxiosResponse } from "axios";
 const CONSTITUENCY_API_BASE_URL: string = "http://localhost:8080/api/constituency";
 
 
-interface Constituency {
-  //todo: fill this interface with typing from the backend model
+export interface Constituency {
+  id: number;
+  name: string;
+
 }
+
 
 class ConstituencyService {
   getConstituency(): Promise<AxiosResponse<Constituency[]>> {
