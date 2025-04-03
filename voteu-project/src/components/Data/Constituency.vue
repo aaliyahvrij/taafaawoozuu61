1e1296e80646eba4be3bc85ed5839b1b5b552f9f
Stephanie Appiah Opoku <stephanie.appiah.opoku@hva.nl>
2025-04-03 09:40:44 +0200

    FEAT: fixed the todo error

--- voteu-project/src/components/Data/Constituency.vue
+++ voteu-project/src/components/Data/Constituency.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
 import { ref, onMounted } from "vue";
 import ConstituencyService from "@/services/ConstituencyService";
+import type { Constituency } from "@/services/ConstituencyService";  // Gebruik 'type' om de interface te importeren
 
-const constituencies = ref([]);
+const constituencies = ref<Constituency[]>([]);
 
 // Fetch constituencies
 const getConstituency = async () => {
   try {
     const response = await ConstituencyService.getConstituency();
-    //todo: fix this error
-    constituencies.value = response.data;
+    constituencies.value = response.data; // Dit zou nu een array van Constituency moeten zijn
   } catch (error) {
     console.error("Error fetching constituencies:", error);
   }
@@ -19,11 +19,10 @@ onMounted(getConstituency);
 </script>
 
 <template>
-  <p v-if="constituencies.length > 0">{{ constituencies[0] }}</p>
+  <p v-if="constituencies.length > 0">{{ constituencies[0].name }} </p> <!-- Zorg ervoor dat je toegang hebt tot een specifiek veld zoals name -->
   <p v-else>Loading...</p>
 </template>
 
 <style scoped>
 /* Add styles if needed */
 </style>
-
