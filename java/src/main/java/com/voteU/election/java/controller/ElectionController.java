93a8736a0df2da3cec0c1cd4bb5dde20a41ecc45
Stephanie Appiah Opoku <stephanie.appiah.opoku@hva.nl>
2025-04-03 09:46:04 +0200

    STYLE: NOTHING CHANGE

--- java/src/main/java/com/voteU/election/java/controller/ElectionController.java
+++ java/src/main/java/com/voteU/election/java/controller/ElectionController.java
@@ -1,29 +1,30 @@
-    package com.voteU.election.java.controller;
+package com.voteU.election.java.controller;
 
-    import com.voteU.election.java.model.Contest;
-    import com.voteU.election.java.model.Party;
-    import com.voteU.election.java.services.ElectionService;
-    import org.springframework.http.ResponseEntity;
-    import org.springframework.web.bind.annotation.*;
-    import java.util.Map;
+import com.voteU.election.java.model.Contest;
+import com.voteU.election.java.model.Party;
+import com.voteU.election.java.services.ElectionService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.Map;
 
-    @RestController
-    @RequestMapping("/election")
-    public class ElectionController {
-        private final ElectionService electionService;
+@RestController
+@RequestMapping("election")
+public class ElectionController {
+    private final ElectionService electionService;
 
-        public ElectionController(ElectionService electionService) {
-            this.electionService = electionService;
-        }
-
-        @GetMapping
-        public Map<String, Map<Integer, Contest>> getElections() {
-            return electionService.getElections();
-        }
-
-        @PostMapping
-        public boolean readResults() {
-            return electionService.readElections();
-        }
-
+    public ElectionController(ElectionService electionService) {
+        this.electionService = electionService;
     }
+
+    @GetMapping
+    public Map<String, Map<Integer, Contest>> getElections() {
+        return electionService.getElections();
+    }
+
+    @PostMapping
+    public boolean readResults() {
+        return electionService.readElections();
+    }
+
+}
+
