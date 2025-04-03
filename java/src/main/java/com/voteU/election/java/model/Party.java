f309ec5765d0a8619ad3c000ae233ad350453ed5
Stephanie Appiah Opoku <stephanie.appiah.opoku@hva.nl>
2025-04-03 09:47:44 +0200

    STYLE: NOTHING CHANGE

--- java/src/main/java/com/voteU/election/java/model/Party.java
+++ java/src/main/java/com/voteU/election/java/model/Party.java
@@ -1,27 +1,22 @@
-package com.voteU.election.java.model;
 
+package com.voteU.election.java.model;
 import java.util.ArrayList;
 import java.util.List;
-
 public class Party {
     int id;
     String name;
     List<Candidate> candidates;
-
     public Party(int id, String name) {
         this.id = id;
         this.name = name;
         this.candidates = new ArrayList<>();
-
     }
     public void addCandidate(Candidate candidate) {
         candidates.add(candidate);
     }
-
     public void setCandidates(List<Candidate> candidates) {
         this.candidates = candidates;
     }
-
     public int getId() {
         return id;
     }
@@ -31,7 +26,6 @@ public class Party {
     public List<Candidate> getCandidates() {
         return candidates;
     }
-
     public String toString() {
         return String.format("Party[id=%d, name=%s]", id, name + ", candidates: " + candidates);
     }
