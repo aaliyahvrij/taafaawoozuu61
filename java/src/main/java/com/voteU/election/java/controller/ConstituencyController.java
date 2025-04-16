f283bf9027ef1aaff74a1111049512328178c222
Stephanie Appiah Opoku <stephanie.appiah.opoku@hva.nl>
2025-04-16 07:22:43 +0200

    FEAT: change the contest to Consistuency

--- java/src/main/java/com/voteU/election/java/controller/ConstituencyController.java
+++ java/src/main/java/com/voteU/election/java/controller/ConstituencyController.java
@@ -1,5 +1,5 @@
 package com.voteU.election.java.controller;
-import com.voteU.election.java.model.Contest;
+import com.voteU.election.java.model.Constituency;
 import com.voteU.election.java.services.ConsistuencyService;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,25 +8,25 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/constituency")
 public class ConstituencyController {
-    private final ConsistuencyService constituencyService = null;
+
     private final ConsistuencyService consistuencyService;
 
     public ConstituencyController(ConsistuencyService consistuencyService) {
         this.consistuencyService = consistuencyService;
     }
 
-//    @GetMapping("/{year}/{contestId}")
-//    public Contest getConstituencyById(@PathVariable String year, @PathVariable int contestId) {
-//        return constituencyService.getConstituency(year, contestId);
-//    }
+    @GetMapping("/{year}/{contestId}")
+    public Constituency getConstituencyById(@PathVariable String year, @PathVariable int contestId) {
+        return consistuencyService.getConstituency(year, contestId);
+    }
 
     @GetMapping
-    public Map<String, Map<Integer, Contest>> getConstituency() {
-        return constituencyService.getElections();
+    public Map<String, Map<Integer, Constituency>> getConstituency() {
+        return consistuencyService.getElections();
     }
 
     @PostMapping
     public boolean readResults() {
-        return constituencyService.readElections();
+        return consistuencyService.readElections();
     }
 }
