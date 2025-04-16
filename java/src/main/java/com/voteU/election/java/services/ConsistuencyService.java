1f1e7a752bd23e886ed22bdeebac47cf1924acf0
Stephanie Appiah Opoku <stephanie.appiah.opoku@hva.nl>
2025-04-16 07:13:32 +0200

    FEAT: change the Map to Consistuency

--- java/src/main/java/com/voteU/election/java/services/ConsistuencyService.java
+++ java/src/main/java/com/voteU/election/java/services/ConsistuencyService.java
@@ -1,9 +1,8 @@
 package com.voteU.election.java.services;
 
-import com.voteU.election.java.model.Contest;
+import com.voteU.election.java.model.Constituency;
 import com.voteU.election.java.reader.DutchElectionReader;
 import org.springframework.stereotype.Service;
-
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,10 +10,10 @@ import java.util.Map;
 @Service
 public class ConsistuencyService {
 
-    private DutchElectionReader electionReader;
-    private final Map<String, Map<Integer, Contest>> storedElections = new HashMap<>();
+    private final DutchElectionReader electionReader;
+    private final Map<String, Map<Integer, Constituency>> storedElections = new HashMap<>();
 
-    public void ElectionService(DutchElectionReader electionReader) {
+    public ConsistuencyService (DutchElectionReader electionReader) {
         this.electionReader = electionReader;
     }
 
@@ -25,8 +24,9 @@ public class ConsistuencyService {
      * @return true if the elections were successfully processed and stored, false otherwise.
      */
     public boolean readElections() {
-        Map<String, Map<Integer, Contest>> elections = electionReader.getElections();
-        if (elections.isEmpty()) {
+        //electionReader.getAll();//
+        Map<String, Map<Integer, Constituency>> elections = electionReader.getConstituencies();
+        if (elections == null || elections.isEmpty())  {
             System.out.println("Geen verkiezingsdata gevonden!");
             return false;
         }
@@ -35,18 +35,19 @@ public class ConsistuencyService {
         return true;
     }
 
+
     /**
      * Retrieves the stored election data (GET equivalent).
      *
      * @return A map containing election results grouped by year.
      */
-    public Map<String, Map<Integer, Contest>> getElections() {
+    public Map<String, Map<Integer, Constituency>> getElections() {
         return storedElections;
     }
 
     //Todo: getConsistuency function
-    public Contest getConstituency(String year, int contestId) {
-        Map<Integer, Contest> contests = storedElections.get(year);
-        return (contests != null) ? contests.get(contestId) : null;
+    public Constituency getConstituency(String year, int constituencyId) {
+        Map<Integer, Constituency> constituencys = storedElections.get(year);
+        return (constituencys != null) ? constituencys.get(constituencyId) : null;
     }
 }
