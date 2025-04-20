d69772e64fb363998f48d60a0570ccb1f0cf6799
Aaliyah <aaliyah.vrij@hva.nl>
2025-04-20 14:47:35 +0200

    FEAT: add getter and setters for partyId

--- java/src/main/java/com/voteU/election/java/model/Candidate.java
+++ java/src/main/java/com/voteU/election/java/model/Candidate.java
@@ -2,6 +2,7 @@ package com.voteU.election.java.model;
 
 public class Candidate {
     int id;
+    int partyId;
     String firstName;
     String lastName;
     int votes;
@@ -13,24 +14,38 @@ public class Candidate {
         this.lastName = lastName;
         this.votes = 0;
     }
+
     public int getId() {
         return id;
     }
+
     public String getFirstName() {
         return firstName;
     }
+
     public String getLastName() {
         return lastName;
     }
+
     public int getVotes() {
         return votes;
     }
+
     public void setVotes(int votes) {
         this.votes = votes;
+    }
+
+    public int getPartyId() {
+        return partyId;
+    }
+
+    public void setPartyId(int partyId) {
+        this.partyId = partyId;
     }
 
     public String toString() {
         return String.format("Candidate[id=%d, firstName=%s, lastName=%s, votes=%d]", id, firstName, lastName, votes);
     }
+
 }
 
