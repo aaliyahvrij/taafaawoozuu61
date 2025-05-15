8b8c3b826a5d8ecb9a55b48f304d5c0d89c62396
Aaliyah <aaliyah.vrij@hva.nl>
2025-05-15 13:21:15 +0200

    ADD: authoritiyId and lombok getter setter

--- java/src/main/java/com/voteU/election/java/model/PollingStation.java
+++ java/src/main/java/com/voteU/election/java/model/PollingStation.java
@@ -1,51 +1,25 @@
 package com.voteU.election.java.model;
 
-import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.List;
+import java.util.Map;
+
+@Setter
+@Getter
 public class PollingStation {
 
-    String id;
-    String name;
-    String zipCode;
-    List<Party> parties;
+    private String id;
+    private String name;
+    private String zipCode;
+    private Map<Integer, Party> parties;
+    private String authorityId;
 
-    public PollingStation(String id, String name, String zipCode, List<Party> parties) {
+    public PollingStation(String id, String name, String zipCode) {
         this.id = id;
         this.name = name;
         this.zipCode = zipCode;
-        this.parties = parties;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
-    }
-
-    public List<Party> getParties() {
-        return parties;
-    }
-
-    public void setParties(List<Party> parties) {
-        this.parties = parties;
     }
 
     @Override
