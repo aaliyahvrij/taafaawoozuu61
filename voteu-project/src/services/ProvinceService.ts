9d466ef14551ab6989595e14def7c488f38ad541
Stephanie Appiah Opoku <stephanie.appiah.opoku@hva.nl>
2025-05-18 17:00:12 +0200

    FEAT: CHANGE THE SERVICE OF THE PROVINCE (FRONTEND)

--- voteu-project/src/services/ProvinceService.ts
+++ voteu-project/src/services/ProvinceService.ts
@@ -1,15 +1,19 @@
-
-import type { Province } from '@/interface/Province.ts'
+import type { Province } from '@/interface/Province'
+import type { Constituency } from '@/interface/Constituency.ts'
+import type { Party } from '@/interface/Party.ts'
 
 export class ProvinceService {
-  static async getAllProvinces(): Promise<Record<number, Province> | null> {
+  static async getProvincesByElection(electionId: string): Promise<Record<number, Province> | null> {
     try {
-      const response = await fetch('http://localhost:8080/api/provinces', {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
+      const response = await fetch(
+        `http://localhost:8080/api/election/TK${electionId}/provinces`,
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
         },
-      })
+      )
 
       if (!response.ok) {
         throw new Error('HTTP error!: ' + response.status)
@@ -21,4 +25,48 @@ export class ProvinceService {
     }
     return null
   }
+  static async getConstituenciesByProvinceId(electionId: string, provinceId: string): Promise<Constituency[] | null> {
+    try {
+      const response = await fetch(
+        `http://localhost:8080/api/election/TK${electionId}/provinces/${provinceId}/constituencies`,
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        },
+      )
+
+      if (!response.ok) {
+        throw new Error('HTTP error!: ' + response.status)
+      }
+
+      return await response.json()
+    } catch (error) {
+      console.error(error)
+    }
+
+    return null
+  }
+  static async getProvincePartyVotes(electionId: string, provinceId: number): Promise<Party[] | null> {
+    try {
+      const response = await fetch(
+        `http://localhost:8080/api/election/TK${electionId}/provinces/${provinceId}/parties`,
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      )
+      if (!response.ok) {
+        throw new Error('HTTP error!: ' + response.status)
+      }
+      return await response.json()
+    } catch (error) {
+      console.error(error)
+      return null
+    }
+  }
 }
+
