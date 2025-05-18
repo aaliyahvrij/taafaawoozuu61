a36915b5e1cd2e0d9b77acf22c968bfef3b09019
Stephanie Appiah Opoku <stephanie.appiah.opoku@hva.nl>
2025-05-18 17:04:46 +0200

    FEAT: remove the provinces add on election model

--- java/src/main/java/com/voteU/election/java/model/Election.java
+++ java/src/main/java/com/voteU/election/java/model/Election.java
@@ -35,30 +35,6 @@ public class Election {
         this.constituencies = new HashMap<>();
         this.authorities = new HashMap<>();
 
-        Province Drenthe = new Province(1, "Drenthe");
-        Province Flevoland = new Province(2, "Flevoland");
-        Province Friesland = new Province(3, "Friesland");
-        Province Gelderland = new Province(4, "Gelderland");
-        Province Groningen = new Province(5, "Groningen");
-        Province Limburg = new Province(6, "Limburg");
-        Province Noord_Brabant  = new Province(7, "Noord-Brabant");
-        Province Noord_Holland  = new Province(8, "Noord-Holland");
-        Province Overijssel = new Province(9, "Overijssel");
-        Province Utrecht = new Province(10, "Utrecht");
-        Province Zeeland = new Province(11, "Zeeland");
-        Province Zuid_Holland  = new Province(12, "Zuid-Holland");
-        provinces.add(Drenthe);
-        provinces.add(Flevoland);
-        provinces.add(Friesland);
-        provinces.add(Gelderland);
-        provinces.add(Groningen);
-        provinces.add(Limburg);
-        provinces.add(Noord_Brabant);
-        provinces.add(Noord_Holland);
-        provinces.add(Overijssel);
-        provinces.add(Utrecht);
-        provinces.add(Zeeland);
-        provinces.add(Zuid_Holland);
     }
 
 
