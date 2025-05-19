bc331bf43ba802c4c0286bf1409922aaaa26927f
Aaliyah <aaliyah.vrij@hva.nl>
2025-05-19 23:13:00 +0200

    FEAT: Add example party comparison chart with configuration and integration in CompareView

---
+++ voteu-project/src/components/Data/charts/PartyChart.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+
+import {partyData} from "@/chartData/partyChartConfig.ts";
+import {onMounted} from "vue";
+import { Chart, registerables, type ChartConfiguration } from 'chart.js';
+
+// Register necessary chart.js components
+Chart.register(...registerables);
+
+
+const partyConfig: ChartConfiguration<'pie', number[], string> = {
+  type: 'pie',
+  data: partyData
+}
+
+onMounted(() => {
+  const partyChartId = document.getElementById('partyChart') as HTMLCanvasElement;
+  if (partyChartId) {
+    new Chart(partyChartId, partyConfig); // Initialize chart with the config
+  }
+});
+
+</script>
+
+<template>
+  <div>
+    <canvas id="partyChart" width="400" height="400"></canvas>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- voteu-project/src/views/CompareView.vue
+++ voteu-project/src/views/CompareView.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
 
+import PartyChart from '@/components/Data/charts/PartyChart.vue'
 </script>
 
 <template>
+
+<PartyChart></PartyChart>
 
 </template>
 
