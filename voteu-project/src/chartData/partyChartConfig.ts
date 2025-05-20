9ce91ae822c9d91aee4e2ba56505f4d6dee3a60d
Aaliyah <aaliyah.vrij@hva.nl>
2025-05-20 16:34:50 +0200

    "Refactor partyChartConfig: simplify dataset and align with Chart.js types"

--- voteu-project/src/chartData/partyChartConfig.ts
+++ voteu-project/src/chartData/partyChartConfig.ts
@@ -1,53 +1,16 @@
 /**
  * example of chart data
  */
+import type { ChartData } from 'chart.js'
 
-export const partyData = {
-  labels: [
-    'VVD',         // Volkspartij voor Vrijheid en Democratie
-    'CDA',         // Christen-Democratisch Appèl
-    'D66',         // Democraten 66
-    'PvdA',        // Partij van de Arbeid
-    'SP',          // Socialistische Partij
-    'PVV',         // Partij voor de Vrijheid
-    'GroenLinks',  // GroenLinks
-    'PvdD',        // Partij voor de Dieren
-    'ChristenUnie', // ChristenUnie
-    'SGP',         // Staatkundig Gereformeerde Partij
-    'FvD',         // Forum voor Democratie
-    'Volt',        // Volt Nederland
-    'BIJ1',        // BIJ1
-    'DENK',        // DENK
-    'JA21',        // JA21
-    'BBB',         // BoerBurgerBeweging
-    'Van Haga',    // Belang van Nederland (Group Van Haga)
 
-  ],
+export const partyData: ChartData<'pie', number[], string> = {
+  labels: [], // string[]
   datasets: [{
     label: 'All the parties currently in the election',
-    data: [35, 15, 24, 9, 9, 17, 8, 6, 5, 3, 5, 3, 1, 2, 3, 1, 1
-    ],
-    backgroundColor: [
-      'rgb(255, 99, 132)', // VVD
-      'rgb(54, 162, 235)', // CDA
-      'rgb(255, 205, 86)', // D66
-      'rgb(75, 192, 192)', // PvdA
-      'rgb(153, 102, 255)', // SP
-      'rgb(255, 159, 64)',  // PVV
-      'rgb(145, 180, 150)', // GroenLinks
-      'rgb(100, 200, 50)',  // PvdD
-      'rgb(200, 120, 230)', // ChristenUnie
-      'rgb(50, 50, 50)',    // SGP
-      'rgb(220, 220, 100)', // FvD
-      'rgb(140, 90, 150)',  // Volt
-      'rgb(255, 215, 0)',   // BIJ1
-      'rgb(250, 80, 180)',  // DENK
-      'rgb(100, 210, 120)', // JA21
-      'rgb(220, 120, 150)', // BBB
-      'rgb(80, 120, 220)',  // Van Haga
-
-    ],
-    hoverOffset: 4
+    data: [], // number[]
+    backgroundColor: [], // string[]
+    hoverOffset: 4,
   }]
 };
 
