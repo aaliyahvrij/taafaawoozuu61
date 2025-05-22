acdd166f430e2388c8ae0a5f2eb19f5ace4195f1
apostoj1 <julian.apostolov@hva.nl>
2025-05-22 15:00:09 +0200

    Last commit just 'contained' the deleted files. Now, the additions. I thought last commit had both. Interesting.

---
+++ voteu-project/src/components/NavBarDesktop.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div id="navbar" class="navbar">
+  <RouterLink to="/">Home</RouterLink>
+  <RouterLink to="/how-does-it-work">How does it work?</RouterLink>
+  <RouterLink to="/filters">Filters</RouterLink>
+  <RouterLink to="/compare">Compare</RouterLink>
+  <RouterLink to="/forum">Forum</RouterLink>
+</div>
+</template>
+
+<style scoped>
+
+</style>
