a0ee359df30a4be127d297fd248e8777dee102a9
apostoj1 <julian.apostolov@hva.nl>
2025-05-28 18:15:51 +0200

    Removed unneeded comment line in front-end Authority.ts file

--- voteu-project/src/interfaces/Authority.ts
+++ voteu-project/src/interfaces/Authority.ts
@@ -3,6 +3,6 @@ import type { Affiliation } from '@/interfaces/Affiliation.ts'
 export interface Authority {
   id: string
   name: string
-  affiliations: { [affId: number]: Affiliation } // Map of parties with partyId as key
+  affiliations: { [affId: number]: Affiliation }
   constId: number
 }
