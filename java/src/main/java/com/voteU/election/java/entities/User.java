925f24ea54d6f6c1d43de4f7999ac95ac27877eb
Aaliyah <aaliyah.vrij@hva.nl>
2025-05-29 17:52:35 +0200

    FEAT: add User entity with JPA annotations and Lombok integration

---
+++ java/src/main/java/com/voteU/election/java/entities/User.java
@@ -0,0 +1,28 @@
+package com.voteU.election.java.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = 'user')
+@Getter
+@Setter
+public class User {
+
+    @GeneratedValue
+    @Id
+    private int id;
+    private String name;
+    private String email;
+    private String first_name;
+    private String last_name;
+    private String password;
+    private String timestamp;
+
+    public User() {
+    }
+}
