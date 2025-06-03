47c25191629ad364f17e12fcd17fd7b6ed67bdbc
Aaliyah <aaliyah.vrij@hva.nl>
2025-06-03 18:19:22 +0200

    FEAT: add CommentsRepository with JPA methods for Comments management

---
+++ java/src/main/java/com/voteU/election/java/repositories/CommentsRepository.java
@@ -0,0 +1,22 @@
+package com.voteU.election.java.repositories;
+
+import com.voteU.election.java.entities.Comments;
+import com.voteU.election.java.entities.Posts;
+import com.voteU.election.java.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Set;
+
+@Repository
+public interface CommentsRepository extends JpaRepository<Comments, Integer> {
+
+    List<Comments> findByPostId(Posts postId);
+
+    List<Comments> findByUserId(User userId);
+
+    List<Comments> findByCommentsId(Integer commentsId);
+
+    void deleteById(Integer id);
+}
