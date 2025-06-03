76218a75533fe9e806d5ba279762f6362407d4c7
Aaliyah <aaliyah.vrij@hva.nl>
2025-06-03 18:19:15 +0200

    FEAT: add CommentsService with methods for Comments management

---
+++ java/src/main/java/com/voteU/election/java/services/CommentsService.java
@@ -0,0 +1,43 @@
+package com.voteU.election.java.services;
+
+import com.voteU.election.java.entities.Comments;
+import com.voteU.election.java.entities.Posts;
+import com.voteU.election.java.entities.User;
+import com.voteU.election.java.repositories.CommentsRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CommentsService {
+
+    private final CommentsRepository commentsRepository;
+
+    public CommentsService(CommentsRepository commentsRepository) {
+        this.commentsRepository = commentsRepository;
+    }
+
+    public List<Comments> getAllComments(){
+        return commentsRepository.findAll();
+    }
+
+    public Comments createComment(Comments comment) {
+        return commentsRepository.save(comment);
+    }
+
+    public List<Comments> getCommentsByPostId(Posts postId){
+        return commentsRepository.findByPostId(postId);
+    }
+
+    public List<Comments> getCommentsByUserId(User userId){
+        return commentsRepository.findByUserId(userId);
+    }
+
+    public List<Comments> getCommentsByCommentsId(Integer commentsId){
+        return commentsRepository.findByCommentsId(commentsId);
+    }
+
+    public void deleteComment(Integer id){
+        commentsRepository.deleteById(id);
+    }
+}
