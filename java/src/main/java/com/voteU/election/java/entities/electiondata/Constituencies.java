39826e0e3aecd1404108f5c7eaa2e6760a4eb3e3
Nigel <agyeman2@hva.nl>
2025-06-05 12:21:07 +0200

    FEAT: NationalPartyvotes and ConstituencyPartyVotes

---
+++ java/src/main/java/com/voteU/election/java/entities/electiondata/Constituencies.java
@@ -0,0 +1,34 @@
+package com.voteU.election.java.entities.electiondata;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "constituencies")
+public class Constituencies {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // Auto-increment primary key
+
+    @Column(name = "constituency_id")
+    private int constituencyId;
+
+    @Column(name = "province_id")
+    private int provinceId;
+
+    @Column(name = "election_id")
+    private int electionId;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "votes")
+    private int votes;
+
+    public Constituencies() {
+    }
+}
---
+++ java/src/main/java/com/voteU/election/java/entities/electiondata/ConstituencyPartyVotes.java
@@ -0,0 +1,31 @@
+package com.voteU.election.java.entities.electiondata;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "constituency_party_votes")
+public class ConstituencyPartyVotes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;  // int, auto-increment primary key
+
+    @Column(name = "constituency_id")
+    private int constituencyId;  // nullable int
+
+    @Column(name = "election_id", length = 255)
+    private String electionId;  // nullable varchar(255)
+
+    @Column(name = "party_id")
+    private int partyId;  // nullable int
+
+    @Column(name = "votes")
+    private int votes;  // nullable int
+
+    public ConstituencyPartyVotes() {
+    }
+}
