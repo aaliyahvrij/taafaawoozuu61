69849874a81632f8ce1f96fb90aec1425f8f32c6
Nigel <agyeman2@hva.nl>
2025-06-06 23:40:08 +0200

    FEAT: add province controller

--- java/src/main/java/com/voteU/election/java/repositories/electiondata/ProvinceRepository.java
+++ java/src/main/java/com/voteU/election/java/repositories/electiondata/ProvinceRepository.java
@@ -4,11 +4,12 @@ import com.voteU.election.java.dto.DropdownOptionDTO;
 import com.voteU.election.java.entities.electiondata.Provinces;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ProvinceRepository extends JpaRepository<Provinces, Integer> {
 
-    @Query("SELECT new com.voteU.election.java.dto.DropdownOptionDTO(c.provinceId, c.name) FROM Provinces c where c.electionId = :electionid")
-    List<DropdownOptionDTO<Integer>> getProvincesByElectionId(String electionId);
+    @Query("SELECT new com.voteU.election.java.dto.DropdownOptionDTO(c.provinceId, c.name) FROM Provinces c where c.electionId = :electionId")
+    List<DropdownOptionDTO<Integer>> getProvincesByElectionId(@Param("electionId") String electionId);
 }
