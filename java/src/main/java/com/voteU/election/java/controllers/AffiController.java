ebf276be251c7f1e2a4b5f6e232bfa78ac689057
apostoj1 <julian.apostolov@hva.nl>
2025-06-07 13:54:39 +0200

    Work done 55

--- java/src/main/java/com/voteU/election/java/controllers/AffiController.java
+++ java/src/main/java/com/voteU/election/java/controllers/AffiController.java
@@ -1,6 +1,7 @@
 package com.voteU.election.java.controllers;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 @RequestMapping("/api/affiliations")
