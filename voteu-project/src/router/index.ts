7c6d9a382fdfb83a6dec9f74bf6c32751f58c59d
Aaliyah <aaliyah.vrij@hva.nl>
2025-06-09 23:20:55 +0200

    FEAT: add admin route and integrate AdminView into router

--- voteu-project/src/router/index.ts
+++ voteu-project/src/router/index.ts
@@ -8,6 +8,7 @@ import CompareView from "@/views/CompareView.vue";
 import AuthView from '@/views/AuthView.vue'
 import login from '@/components/login.vue'
 import register from '@/components/register.vue'
+import AdminView from '@/views/AdminView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -58,6 +59,12 @@ const router = createRouter({
       path:'/register',
       name:'register',
       component: register,
+    },
+
+    {
+      path:'/admin',
+      name: 'admin',
+      component: AdminView,
     },
   ],
 })
