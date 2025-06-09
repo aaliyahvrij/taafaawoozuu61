8875970ffd49c17946efac671aeb950693590ebb
apostoj1 <julian.apostolov@hva.nl>
2025-06-09 19:30:51 +0200

    Work done 67

--- java/src/main/java/com/voteU/election/java/controllers/PoStController.java
+++ java/src/main/java/com/voteU/election/java/controllers/PoStController.java
@@ -19,7 +19,7 @@ public class PoStController {
     }
 
     @GetMapping
-    public LinkedHashMap<String, PollingStation> getNationalLevel_poStOf(@PathVariable String electionId) {
+    public LinkedHashMap<String, PollingStation> getNationalLevel_pollingStationsOf(@PathVariable String electionId) {
         return this.poStService.getNationalLevel_pollingStationsOf(electionId);
     }
 }
