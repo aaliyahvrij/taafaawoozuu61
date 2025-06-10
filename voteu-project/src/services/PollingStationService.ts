fe4b91ab02df3af1fabd0143b36b7fd9dff91e60
Nigel <agyeman2@hva.nl>
2025-06-10 12:47:13 +0200

    CHORE: all service files use api fetch

--- voteu-project/src/services/PollingStationService.ts
+++ voteu-project/src/services/PollingStationService.ts
@@ -1,45 +1,36 @@
-
-import type { Party } from '@/interface/Party.ts'
-import type { PollingStation } from '@/interface/PollingStation.ts'
+import type { Party } from '@/interface/Party.ts';
+import type { PollingStation } from '@/interface/PollingStation.ts';
+import { apiFetch } from '@/services/api.ts'
 
 export class PollingStationService {
-  static async getPollingStationsByAuthorityId(electionId: string, constituencyId: string , authorityId: string): Promise<Record<string, PollingStation> | null> {
+  static async getPollingStationsByAuthorityId(
+    electionId: string,
+    constituencyId: string,
+    authorityId: string
+  ): Promise<Record<string, PollingStation> | null> {
     try {
-      const response = await fetch
-      (`http://localhost:8080/api/election/TK${electionId}/constituencies/${constituencyId}/authorities/${authorityId}/pollingStations`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      })
-
-      if (!response.ok) {
-        throw new Error('HTTP error!: ' + response.status)
-      }
-      return await response.json()
+      return await apiFetch<Record<string, PollingStation>>(
+        `/election/TK${electionId}/constituencies/${constituencyId}/authorities/${authorityId}/pollingStations`
+      );
     } catch (error) {
-      console.error(error)
+      console.error(error);
+      return null;
     }
-    return null
   }
 
-  static async getPollingStationVotesByAuthorityId(electionId: string, constituencyId: string, authorityId: string, pollingStationId: string): Promise<Record<number, Party> | null> {
+  static async getPollingStationVotesByAuthorityId(
+    electionId: string,
+    constituencyId: string,
+    authorityId: string,
+    pollingStationId: string
+  ): Promise<Record<number, Party> | null> {
     try {
-      const response = await fetch
-      (`http://localhost:8080/api/election/TK${electionId}/constituencies/${constituencyId}/authorities/${authorityId}/pollingStations/${pollingStationId}/parties`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      })
-
-      if (!response.ok) {
-        throw new Error('HTTP error!: ' + response.status)
-      }
-      return await response.json()
+      return await apiFetch<Record<number, Party>>(
+        `/election/TK${electionId}/constituencies/${constituencyId}/authorities/${authorityId}/pollingStations/${pollingStationId}/parties`
+      );
     } catch (error) {
-      console.error(error)
+      console.error(error);
+      return null;
     }
-    return null
   }
 }
