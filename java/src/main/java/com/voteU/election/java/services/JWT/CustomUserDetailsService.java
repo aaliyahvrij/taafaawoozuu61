e020922b25ef5848fc4d43c6a1f024a93bd31763
Aaliyah <aaliyah.vrij@hva.nl>
2025-06-10 14:35:40 +0200

    FEAT: add bean for better spring file tracking

--- java/src/main/java/com/voteU/election/java/services/JWT/CustomUserDetailsService.java
+++ java/src/main/java/com/voteU/election/java/services/JWT/CustomUserDetailsService.java
@@ -3,10 +3,13 @@ package com.voteU.election.java.services.JWT;
 import com.voteU.election.java.entities.CustomUserDetails;
 import com.voteU.election.java.entities.User;
 import com.voteU.election.java.repositories.UserRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service("userDetailsService")
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
