b6b83c0507260d07aa6e16270493d076f2b6c496
Nigel <agyeman2@hva.nl>
2025-06-10 12:41:25 +0200

    CHORE: change election service frontend

--- voteu-project/src/services/ElectionService.ts
+++ voteu-project/src/services/ElectionService.ts
@@ -1,52 +1,25 @@
-import type {Election} from "@/interface/Election.ts";
-import type { Party } from '@/interface/Party.ts'
-
+import type { Election } from "@/interface/Election.ts";
+import type { Party } from '@/interface/Party.ts';
+import { apiFetch } from '@/services/api.ts'
 
 export class ElectionService {
-  static async getElection(electionId : string): Promise<Election | null> {
+  static async getElection(electionId: string): Promise<Election | null> {
     try {
-      const response = await fetch(
-        `http://localhost:8080/api/election/TK${electionId}`,
-        {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-          }
-        });
-
-      if (!response.ok) {
-        throw new Error('HTTP error!: ' + response.status);
-      }
-      return await response.json();
-    }
-
-    catch (error) {
-      console.error(error)
-    }
-    return null;
-  }
-
-
-  static async getNationalPartyVotes(electionId : string): Promise<Record<number, Party> | null> {
-      try {
-        const response = await fetch(
-          `http://localhost:8080/api/election/TK${electionId}/parties`,
-          {
-            method: 'GET',
-            headers: {
-              'Content-Type': 'application/json',
-            }
-          });
-
-        if (!response.ok) {
-          throw new Error('HTTP error!: ' + response.status);
-        }
-        return await response.json();
-      }
-
-      catch (error) {
-        console.error(error)
-      }
+      const election = await apiFetch<Election>(`/election/TK${electionId}`);
+      return election;
+    } catch (error) {
+      console.error(error);
       return null;
     }
   }
+
+  static async getNationalPartyVotes(electionId: string): Promise<Record<number, Party> | null> {
+    try {
+      const parties = await apiFetch<Record<number, Party>>(`/election/TK${electionId}/parties`);
+      return parties;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  }
+}
