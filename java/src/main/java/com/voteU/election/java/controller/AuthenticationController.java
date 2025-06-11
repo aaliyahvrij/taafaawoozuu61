798846db489084dece789bc2c262c157a73062ca
Aaliyah <aaliyah.vrij@hva.nl>
2025-06-11 21:39:34 +0200

    FEAT: document AuthenticationController endpoints with OpenAPI annotations

--- java/src/main/java/com/voteU/election/java/controller/AuthenticationController.java
+++ java/src/main/java/com/voteU/election/java/controller/AuthenticationController.java
@@ -3,6 +3,9 @@ package com.voteU.election.java.controller;
 import com.voteU.election.java.dto.JwtResponse;
 import com.voteU.election.java.dto.LoginDTO;
 import com.voteU.election.java.services.AuthenticationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +22,12 @@ public class AuthenticationController {
         this.authenticationService = authenticationService;
     }
 
+    @Operation(summary = "Authenticate user", description = "Authenticate user by checking and unhashing the login credentials")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "User logged in successfully"),
+            @ApiResponse(responseCode = "404", description = "User not found."),
+            @ApiResponse(responseCode = "403", description = "unauthorized")
+    })
     @PostMapping("/login")
     public ResponseEntity<JwtResponse> authenticateUser(@RequestBody LoginDTO loginDTO) {
         JwtResponse jwtResponse = authenticationService.authenticate(loginDTO);
