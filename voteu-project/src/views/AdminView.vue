fe9704c9d90196b37ffc7038e99fe1e4a3ba63db
Stephanie Appiah Opoku <stephanie.appiah.opoku@hva.nl>
2025-06-27 22:57:28 +0200

    FEAT: add report table component in AdminView.vue

--- voteu-project/src/views/AdminView.vue
+++ voteu-project/src/views/AdminView.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
 
 import UserTable from '@/components/Data/user/userTable.vue'
+import ReportTable from '@/components/Data/user/ReportTable.vue'
 </script>
-
 <template>
-  <user-table></user-table>
+  <div>
+    <user-table />
+    <hr />
+    <report-table />
+  </div>
 </template>
 
 <style scoped>
