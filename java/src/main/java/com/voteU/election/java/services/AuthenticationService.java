b8f4b01ce7a37a7d3081fbec3ddab8c97e5af90b
Stephanie Appiah Opoku <stephanie.appiah.opoku@hva.nl>
2025-06-30 17:50:45 +0200

    FEAT: add authentication for user block in the service

--- java/src/main/java/com/voteU/election/java/services/AuthenticationService.java
+++ java/src/main/java/com/voteU/election/java/services/AuthenticationService.java
@@ -3,6 +3,7 @@ package com.voteU.election.java.services;
 import com.voteU.election.java.dto.JwtResponse;
 import com.voteU.election.java.dto.LoginDTO;
 import com.voteU.election.java.entities.User;
+import com.voteU.election.java.exceptions.AccessDeniedException;
 import com.voteU.election.java.repositories.UserRepository;
 import com.voteU.election.java.services.JWT.JwtService;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -29,12 +30,14 @@ public class AuthenticationService {
         User user = userRepository.findUserByUsername(request.getUsername())
                 .orElseThrow(() -> new UsernameNotFoundException("Invalid credentials"));
 
+        if (user.isBlocked()) {
+            throw new AccessDeniedException("User is blocked");
+        }
+
         if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
             throw new BadCredentialsException("Invalid credentials");
         }
-
         String token = jwtService.generateToken(user);
-
         return new JwtResponse(token);
     }
 }
