0a256e71d81286bb492a862deb93461400edb84f
Stephanie Appiah Opoku <stephanie.appiah.opoku@hva.nl>
2025-06-30 17:54:31 +0200

    FEAT: add post in the interface Report.ts

--- voteu-project/src/interface/Report.ts
+++ voteu-project/src/interface/Report.ts
@@ -1,7 +1,12 @@
 export interface Report {
   id: number
-  reporter: { id: number }
-  reported: { id: number }
+  reporter: { id: number, username: string }
+  reported: { id: number, username: string }
   reason: string
   created_at: string
+  post?: {
+    id: number
+    title: string
+    body: string
+  }
 }
