2066f1ff414146baff56186bf729ddd419991e19
apostoj1 <julian.apostolov@hva.nl>
2025-10-31 23:12:47 +0100

    Work done 131

--- voteu-project/src/chartData/affiChartConfig.ts
+++ voteu-project/src/chartData/affiChartConfig.ts
@@ -5,12 +5,12 @@ import type { ChartData } from 'chart.js'
 
 export const affiData: ChartData<'pie', number[], string> = {
   labels: [], // string[]
-  datasets: [{
-    label: 'All the affiliations currently in the election',
-    data: [], // number[]
-    backgroundColor: [], // string[]
-    hoverOffset: 4,
-  }]
-};
-
-
+  datasets: [
+    {
+      label: 'All the affiliations currently in the election',
+      data: [], // number[]
+      backgroundColor: [], // string[]
+      hoverOffset: 4,
+    },
+  ],
+}
